7c1336710f4176276f2eebf7a740e5a2a60797e0
Paolo Meridiani <paolo.meridiani@cern.ch>
2015-06-04 12:03:05 +0200

    Add getters

--- EcalCalibDataFormats/interface/PhiSymInfo.h
+++ EcalCalibDataFormats/interface/PhiSymInfo.h
@@ -19,18 +19,22 @@ public:
     //---dtor---
     ~PhiSymInfo();
 
+    //---set------
+    void            setStartLumi(edm::LuminosityBlock const& lumi);
+    void            setEndLumi(edm::LuminosityBlock const& lumi);
+
     //---getters---
     inline uint64_t GetTotHitsEB()  const {return totHitsEB_;};
     inline uint64_t GetTotHitsEE()  const {return totHitsEE_;};
     inline uint32_t GetNEvents()    const {return nEvents_;};
     float           GetMean(char k)      const;
     float           GetMeanSigma(char k) const;
-
-    void            setStartLumi(edm::LuminosityBlock const& lumi);
-    void            setEndLumi(edm::LuminosityBlock const& lumi);
+    inline edm::LuminosityBlockID getStartLumi() const { return startLumi_; };
+    inline edm::LuminosityBlockID getEndLumi() const { return endLumi_; };
 
     //---utils---
     void            Update(const reco::BeamSpot* bs, uint64_t& nEB, uint64_t& nEE);
+
 
     //---operators---
     friend std::ostream& operator<<(std::ostream& out, const PhiSymInfo& obj);
