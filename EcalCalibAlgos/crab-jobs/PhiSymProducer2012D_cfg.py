7000fe7e6b30cb762302de20b19448b351c31d74
Simone Pigazzi <simone.pigazzini@live.it>
2015-06-21 14:15:54 +0200

    crab cfg for Run2012D

---
+++ EcalCalibAlgos/crab-jobs/PhiSymProducer2012D_cfg.py
@@ -0,0 +1,89 @@
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+from Configuration.AlCa.GlobalTag import GlobalTag
+
+# parse commad line options
+options = VarParsing('analysis')
+options.maxEvents = -1
+options.outputFile = 'phisym_producer_1lumis.root'
+options.parseArguments()
+
+process=cms.Process("PHISYM")
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+process.load('Configuration.Geometry.GeometryExtended2015Reco_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load('RecoLocalCalo.EcalRecProducers.ecalMultiFitUncalibRecHit_cfi')
+process.load('RecoLocalCalo.EcalRecProducers.ecalUncalibRecHit_cfi')
+process.load('RecoLocalCalo.EcalRecProducers.ecalRecHit_cfi')
+process.load("RecoVertex.BeamSpotProducer.BeamSpot_cff")
+
+process.load('FWCore/MessageService/MessageLogger_cfi')
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.MessageLogger.cerr.default = cms.untracked.PSet(
+    limit = cms.untracked.int32(10000000),
+    reportEvery = cms.untracked.int32(5000)
+)
+
+# import of standard configurations
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(options.maxEvents)
+)
+
+# skip bad events
+process.options = cms.untracked.PSet(
+    SkipEvent = cms.untracked.vstring('ProductNotFound'),
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+                            inputCommands = cms.untracked.vstring(
+                                'keep *',
+                                'drop *_hltEcalDigis_*_*',
+                                'drop *_hltTriggerSummaryAOD_*_*'
+                            ),
+                            fileNames = cms.untracked.vstring(
+                                "/store/data/Run2012D/AlCaPhiSym/RAW/v1/000/203/774/B492587E-FF08-E211-953F-BCAEC53296FB.root")
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.20 $'),
+    annotation = cms.untracked.string('step_PHISYM nevts:'+str(options.maxEvents)),
+    name = cms.untracked.string('PhiSymProducer')
+)
+
+# PHISYM producer
+process.load('PhiSym.EcalCalibAlgos.PhiSymProducer_cfi')
+# process.PhiSymProducer.applyEtThreshold=cms.bool(False)
+process.PhiSymProducer.makeSpectraTreeEB = False
+process.PhiSymProducer.makeSpectraTreeEE = False
+process.PhiSymProducer.barrelHitCollection = cms.InputTag('hltAlCaPhiSymUncalibrator', 'phiSymEcalRecHitsEB', 'HLT')
+process.PhiSymProducer.endcapHitCollection = cms.InputTag('hltAlCaPhiSymUncalibrator', 'phiSymEcalRecHitsEE', 'HLT')
+
+# Output definition
+PHISYM_output_commands = cms.untracked.vstring(
+    "drop *",
+    "keep *_PhiSymProducer_*_*")
+
+process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
+                                         splitLevel = cms.untracked.int32(0),
+                                         outputCommands = PHISYM_output_commands,
+                                         fileName = cms.untracked.string(options.outputFile)
+)
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string("phisym_spectra.root"))
+
+# GLOBAL-TAG
+process.GlobalTag = GlobalTag(process.GlobalTag, 'GR_R_74_V12A')
+
+# SCHEDULE
+process.path = cms.Path(process.offlineBeamSpot)
+process.path *= process.PhiSymProducer
+
+process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
+
+process.schedule = cms.Schedule(process.path, process.RECOSIMoutput_step)
