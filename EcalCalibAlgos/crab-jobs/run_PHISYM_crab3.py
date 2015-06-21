7e459f3db8985b289b8d3337851c52283b340f69
Simone Pigazzi <simone.pigazzini@live.it>
2015-06-21 14:13:30 +0200

    cleanup

--- EcalCalibAlgos/crab-jobs/run_PHISYM_crab3.py
+++ EcalCalibAlgos/crab-jobs/run_PHISYM_crab3.py
@@ -3,7 +3,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_('General')
-config.General.requestName     = 'PHISYM-test_production_GR_P_V56-Run2015A_v4'
+config.General.requestName     = 'PHISYM-CMSSW_741-GR_R_74_V12A-Run2012D_v1'
 config.General.transferLogs    = True
 config.General.transferOutputs = True
 
@@ -11,20 +11,21 @@ config.section_('JobType')
 config.JobType.pluginName      = 'Analysis'
 
 # Name of the CMSSW configuration file
-config.JobType.psetName        = 'PhiSymProducer_cfg.py'
+config.JobType.psetName        = 'PhiSymProducer2012D_cfg.py'
 config.JobType.priority        = 20
+config.JobType.maxJobRuntimeMin= 3000
 
 config.section_('Data')
 # This string determines the primary dataset of the newly-produced outputs.
-config.Data.inputDataset       = '/AlCaPhiSym/Run2015A-v1/RAW'
+config.Data.inputDataset       = '/AlCaPhiSym/Run2012D-v1/RAW'
 #config.Data.useParent = True
 config.Data.inputDBS           = 'global'
 config.Data.splitting          = 'LumiBased'
-config.Data.lumiMask           = 'Run2015A_v0_filtered.json'
-config.Data.unitsPerJob        = 50
+config.Data.lumiMask           = 'json_Golden.txt'
+config.Data.unitsPerJob        = 200
 config.Data.totalUnits         = -1
 config.Data.publication        = True
-config.Data.isbchecksum        = 'aa6d0f694fbde2c60e6338a92e82f36e'
+#config.Data.isbchecksum        = 'aa6d0f694fbde2c60e6338a92e82f36e'
 #config.Data.ignoreLocality     = True
 
 # This string is used to construct the output dataset name
--- EcalCalibAlgos/test/PhiSymProducer2012D_cfg.py
+++ EcalCalibAlgos/test/PhiSymProducer2012D_cfg.py
@@ -10,7 +10,6 @@ options.parseArguments()
 
 process=cms.Process("PHISYM")
 
-#process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 process.load('Configuration.Geometry.GeometryExtended2015Reco_cff')
 process.load('Configuration.StandardSequences.L1Reco_cff')
@@ -47,8 +46,6 @@ process.source = cms.Source("PoolSource",
                             ),
                             fileNames = cms.untracked.vstring(
                                 "/store/data/Run2012D/AlCaPhiSym/RAW/v1/000/203/774/B492587E-FF08-E211-953F-BCAEC53296FB.root")
-                                # "root://cmsxrootd-site.fnal.gov//store/data/Run2015A/AlCaPhiSym/RAW/v1/000/247/720/00000/4C0AF78B-4810-E511-8C09-02163E0143CB.root",
-                                # "root://cmsxrootd-site.fnal.gov//store/data/Run2015A/AlCaPhiSym/RAW/v1/000/247/795/00000/E001FA5B-D510-E511-A5B8-02163E0135FD.root")
 )
 
 # Production Info
@@ -57,44 +54,6 @@ process.configurationMetadata = cms.untracked.PSet(
     annotation = cms.untracked.string('step_PHISYM nevts:'+str(options.maxEvents)),
     name = cms.untracked.string('PhiSymProducer')
 )
-
-isStream=True
-runMultiFit=False
-isBX50ns=True
-
-if (runMultiFit):
-    if (isBX50ns):
-        process.ecalMultiFitUncalibRecHit.algoPSet = cms.PSet(
-            useLumiInfoRunHeader = cms.bool(False),
-            activeBXs = cms.vint32(-4,-2,0,2,4)
-        )
-    else:
-        process.ecalMultiFitUncalibRecHit.algoPSet = cms.PSet(
-            useLumiInfoRunHeader = cms.bool(False),
-            activeBXs = cms.vint32(-5,-4,-3,-2,-1,0,1,2,3,4)
-        ) 
-
-#ecalUncalibRecHit
-if (isStream):
-    process.ecalUncalibRecHit.EBdigiCollection = cms.InputTag("hltEcalPhiSymFilter","phiSymEcalDigisEB")
-    process.ecalUncalibRecHit.EEdigiCollection = cms.InputTag("hltEcalPhiSymFilter","phiSymEcalDigisEE")
-
-#ecalMultiFitUncalibRecHit
-if (isStream):
-    process.ecalMultiFitUncalibRecHit.EBdigiCollection = cms.InputTag("hltEcalPhiSymFilter","phiSymEcalDigisEB")
-    process.ecalMultiFitUncalibRecHit.EEdigiCollection = cms.InputTag("hltEcalPhiSymFilter","phiSymEcalDigisEE")
-
-#ecalRecHit (no ricovery)
-process.ecalRecHit.killDeadChannels = cms.bool( False )
-process.ecalRecHit.recoverEBVFE = cms.bool( False )
-process.ecalRecHit.recoverEEVFE = cms.bool( False )
-process.ecalRecHit.recoverEBFE = cms.bool( False )
-process.ecalRecHit.recoverEEFE = cms.bool( False )
-process.ecalRecHit.recoverEEIsolatedChannels = cms.bool( False )
-process.ecalRecHit.recoverEBIsolatedChannels = cms.bool( False )
-if (not runMultiFit):
-    process.ecalRecHit.EBuncalibRecHitCollection = cms.InputTag("hltAlCaPhiSymUncalibrator","phiSymEcalRecHitsEB")
-    process.ecalRecHit.EEuncalibRecHitCollection = cms.InputTag("hltAlCaPhiSymUncalibrator","phiSymEcalRecHitsEE")
 
 # PHISYM producer
 process.load('PhiSym.EcalCalibAlgos.PhiSymProducer_cfi')
@@ -122,23 +81,9 @@ process.TFileService = cms.Service("TFileService",
 process.GlobalTag = GlobalTag(process.GlobalTag, 'GR_R_74_V12A')
 
 # SCHEDULE
-if (not runMultiFit):
-    process.reconstruction_step = cms.Sequence( process.ecalUncalibRecHit + process.ecalRecHit )
-else:
-    process.reconstruction_step = cms.Sequence( process.ecalMultiFitUncalibRecHit + process.ecalRecHit )
-
-if (isStream):
-    process.p = cms.Path(process.reconstruction_step)
-    process.p *= process.offlineBeamSpot
-    process.p *= process.PhiSymProducer
-else:
-    process.p = cms.Path(process.RawToDigi) 
-    process.p *= process.L1Reco
-    process.p *= process.reconstruction_step
-    process.p *= process.offlineBeamSpot
-    process.p *= process.PhiSymProducer
-
-process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
 process.path = cms.Path(process.offlineBeamSpot)
 process.path *= process.PhiSymProducer
+
+process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
+
 process.schedule = cms.Schedule(process.path, process.RECOSIMoutput_step)
