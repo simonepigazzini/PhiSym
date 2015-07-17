c7930c672ea0690fd17ed7af85a3374898c1b5ab
Simone Pigazzi <simone.pigazzini@live.it>
2015-07-17 11:01:39 +0200

    tracking IC files in EcalCalibAlgos/data/

--- EcalCalibAlgos/test/drawSimpleChecks.py
+++ EcalCalibAlgos/test/drawSimpleChecks.py
@@ -18,7 +18,7 @@ from DataFormats.FWLite import Handle, Events, Lumis
 
 # open file (you can use 'edmFileUtil -d /store/whatever.root' to get the physical file name)
 #lumis = Lumis("file:phisym.root")
-lumis = Lumis("file:/afs/cern.ch/user/s/spigazzi/work/ECAL/CMSSW_7_4_1/src/PhiSym/EcalCalibAlgos/phisym.root")
+lumis = Lumis("root://xrootd-cms.infn.it//store/user/spigazzi/AlCaPhiSym/crab_PHISYM-CMSSW_741-weights-GR_P_V56-Run2015B_v1/150714_150558/0000/phisym_weights_1lumis_59.root")
 
 handlePhiSymInfo  = Handle ("std::vector<PhiSymInfo>")
 handlePhiSymRecHitsEB  = Handle ("std::vector<PhiSymRecHit>")
@@ -31,6 +31,9 @@ histos={}
 
 histos["EB_OccupancyMap"]=ROOT.TH2F("EB_OccupancyMap","EB_OccupancyMap",360,0.5,360.5,171,-85.5,85.5)
 histos["EB_EtMap"]=ROOT.TH2F("EB_EtMap","EB_EtMap",360,0.5,360.5,171,-85.5,85.5)
+histos["EB_EtMeanMap"]=ROOT.TH2F("EB_EtMeanMap","EB_EtMeanMap",360,0.5,360.5,171,-85.5,85.5)
+histos["EB_LCSumMap"]=ROOT.TH2F("EB_LCSumMap","EB_LCSumMap",360,0.5,360.5,171,-85.5,85.5)
+histos["EB_LCMap"]=ROOT.TH2F("EB_LCMap","EB_LCMap",360,0.5,360.5,171,-85.5,85.5)
 
 histos["EEM_OccupancyMap"]=ROOT.TH2F("EEM_OccupancyMap","EEM_OccupancyMap",100,0.5,100.5,100,0.5,100.5)
 histos["EEM_EtMap"]=ROOT.TH2F("EEM_EtMap","EEM_EtMap",100,0.5,100.5,100,0.5,100.5)
@@ -56,17 +59,26 @@ for i,lumi in enumerate(lumis):
 
     for hit in phiSymRecHitsEB:
         myId=ROOT.EBDetId(hit.GetRawId())
-        histos["EB_EtMap"].Fill(myId.iphi(),myId.ieta(),hit.GetSumEt(0))
         histos["EB_OccupancyMap"].Fill(myId.iphi(),myId.ieta(),hit.GetNhits())
+        histos["EB_EtMap"].Fill(myId.iphi(),myId.ieta(),hit.GetSumEt(0))
+        histos["EB_LCSumMap"].Fill(myId.iphi(),myId.ieta(),hit.GetLCSum())
     for hit in phiSymRecHitsEE:
         myId=ROOT.EEDetId(hit.GetRawId())
         if (myId.zside()<0):
-            histos["EEM_EtMap"].Fill(myId.ix(),myId.iy(),hit.GetSumEt(0))
             histos["EEM_OccupancyMap"].Fill(myId.ix(),myId.iy(),hit.GetNhits())
+            histos["EEM_EtMap"].Fill(myId.ix(),myId.iy(),hit.GetSumEt(0))
         else:
-            histos["EEP_EtMap"].Fill(myId.ix(),myId.iy(),hit.GetSumEt(0))
             histos["EEP_OccupancyMap"].Fill(myId.ix(),myId.iy(),hit.GetNhits())
+            histos["EEP_EtMap"].Fill(myId.ix(),myId.iy(),hit.GetSumEt(0))
 
+for iPhi in range(1, 361):
+    for iEta in range(1, 172):
+        iBin = histos["EB_LCMap"].GetBin(iPhi, iEta)
+        nHits = histos["EB_OccupancyMap"].GetBinContent(iBin)
+        if nHits > 0:
+            histos["EB_EtMeanMap"].SetBinContent(iPhi, iEta, histos["EB_EtMap"].GetBinContent(iBin)/nHits)
+            histos["EB_LCMap"].SetBinContent(iPhi, iEta, histos["EB_LCSumMap"].GetBinContent(iBin)/nHits)
+            
 outFile=ROOT.TFile("phiSymStreamCheck.root","RECREATE")
 for histo in histos.keys():
     histos[histo].Write()
