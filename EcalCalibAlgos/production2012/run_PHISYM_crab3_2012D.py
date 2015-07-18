f5c1f0912e8a2b1bc54fa809186fac51a496e0dc
Simone Pigazzi <simone.pigazzini@live.it>
2015-07-18 23:19:32 +0200

    2012D newThr good rings EE

--- EcalCalibAlgos/production2012/run_PHISYM_crab3_2012D.py
+++ EcalCalibAlgos/production2012/run_PHISYM_crab3_2012D.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 
 config = Configuration()
 config.section_('General')
-config.General.requestName = 'PHISYM-CMSSW_741_newTHR-GR_P_V56-Run2012D_v3'
+config.General.requestName = 'PHISYM-CMSSW_746_newTHR-74X_dataRun2_Prompt_v0-Run2012D_v1'
 config.General.transferLogs = True
 config.General.transferOutputs = True
 config.section_('JobType')
