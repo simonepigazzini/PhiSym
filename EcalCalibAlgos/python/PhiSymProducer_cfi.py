4e0914caf2e3d219d33748c0357e04869b71e36d
Simone Pigazzi <simone.pigazzini@live.it>
2016-09-13 14:57:46 +0200

    update EE thresholds, values set by heart

--- EcalCalibAlgos/python/PhiSymProducer_cfi.py
+++ EcalCalibAlgos/python/PhiSymProducer_cfi.py
@@ -1,5 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
+# - thrEEmod is the value in ADC count that rappresent the "5 sigma noise" threshold 
+# - A*ring + B is a ad-hoc parametrization of the equivalent noise level, in this way the thr is set in MeV
+
 PhiSymProducer = cms.EDProducer(
     "PhiSymProducer",
     barrelHitCollection = cms.InputTag('ecalRecHit', 'EcalRecHitsEB', 'PHISYM'),
@@ -8,9 +11,9 @@ PhiSymProducer = cms.EDProducer(
     eThreshold_barrel = cms.double(10*1.75*0.04),    
     etCut_barrel = cms.double(1), #this is actually summed to eThr in order to define the upper bound    
     etCut_endcap = cms.double(1), #this is actually summed to eThr in order to define the upper bound    
-    A = cms.vdouble(4.87, 35.42), # B + A*ring 2016 thr are defined as two linear cut (one for iring<30 and one above)
-    B = cms.vdouble(98.12, -787.79),
-    thrEEmod = cms.double(12.),
+    A = cms.vdouble(10, 100), # B + A*ring 2016 thr are defined as two linear cut (one for iring<30 and one above)
+    B = cms.vdouble(150, -2500),
+    thrEEmod = cms.double(10.),
     nMisCalib = cms.int32(10), # <= 10; even; central value does not count
     misCalibRangeEB = cms.vdouble(0.95, 1.05),
     misCalibRangeEE = cms.vdouble(0.90, 1.10),
