be26b577f98578ba217fe518baf31bd25bf9a61a
Simone Pigazzi <simone.pigazzini@live.it>
2017-10-30 19:07:02 +0100

    Auto resub of calibration jobs

--- EcalCalibAlgos/scripts/submitBatch.py
+++ EcalCalibAlgos/scripts/submitBatch.py
@@ -7,14 +7,16 @@ from commands import getstatusoutput
 from commands import getoutput
 import datetime
 import argparse
+import subprocess
 
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
 # ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
-def lxbatchSubmitJob (eosdir, iovmap, iov, basedir, outdir, queue, dryrun):
+def lxbatchSubmitJob(eosdir, iovmap, iov, basedir, outdir, queue, dryrun):
     jobname = outdir+'/PhiSymCalibration_'+queue+'_'+str(iov)+'.sh'
+    logname = jobname.replace('.sh', '.log')
     f = open (jobname, 'w')
     f.write ('#!/bin/sh' + '\n\n')
     f.write ('export X509_USER_PROXY=/afs/cern.ch/user/s/spigazzi/x509up_u68758 \n\n')
@@ -29,8 +31,28 @@ def lxbatchSubmitJob (eosdir, iovmap, iov, basedir, outdir, queue, dryrun):
     f.close ()
     getstatusoutput ('chmod 755 ' + jobname)
     if not dryrun:
-        getstatusoutput ('cd '+outdir+'; bsub -q ' + queue + ' ' + '-u simone.pigazzini@cern.ch ' + jobname + '; cd -')
+        getstatusoutput('cd '+outdir+'; bsub -q ' + queue + ' -oo ' + logname + ' -u "" ' + jobname + '; cd -')
 
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+def resubmitJobs(outdir, queue):
+    """
+    Check log files in directory outdir and resubmit failed jobs
+    """
+
+    getFailedCmd = subprocess.Popen(
+        ['grep -r "Exited with exit code" '+outdir+' | sed -e s/:.*$//g'],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    logs_list, err = getFailedCmd.communicate()
+    logs_list = logs_list.split('\n')
+    logs_list.pop()
+    if len(logs_list) == 0:
+        print("No job to resubmit, calibration complete!")
+    for log in logs_list:
+        jobname = log.replace('.log', '.sh')
+        print log
+        getstatusoutput ('cd '+outdir+'; bsub -q ' + queue + ' -oo ' + log + ' -u "" ' + jobname + '; cd -')
+        
 # ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
 if __name__ == '__main__':
@@ -40,7 +62,9 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--iovmap' , default = '', help='ROOT file with IOV definition')
     parser.add_argument('-q', '--queue' , default = '8nm', help='batch queue (1nh)')
     parser.add_argument('-o', '--outdir' , default = '$CMSSW_BASE/src/PhiSym/EcalCalibAlgos/ntuples/', help='storage path')
+    parser.add_argument('--resub' , action="store_true", default=False, help='resubmit failed jobs, requires -o')
     parser.add_argument('--dryrun' , action="store_true", default=False, help='do not submit the jobs, just create them')
+    
     
     args = parser.parse_args ()
 
@@ -49,13 +73,17 @@ if __name__ == '__main__':
     print("Output will be copied to: "+stage_out_dir)
     getstatusoutput('mkdir -p '+stage_out_dir)
 
-    ## get list of iovs.
-    iovfile = ROOT.TFile.Open(args.iovmap, "READ")
-    iovtree = iovfile.Get("outTree_barl")
-    n_jobs = iovtree.GetEntries()
+    ## resubmit failed jobs
+    if args.resub:
+        resubmitJobs(stage_out_dir, args.queue)
+    else:
+        ## get list of iovs.
+        iovfile = ROOT.TFile.Open(args.iovmap, "READ")
+        iovtree = iovfile.Get("iov_map")
+        n_jobs = iovtree.GetEntries()
 
-    ## create jobs
-    basedir = os.path.expandvars("$CMSSW_BASE")
-    print("Submitting "+str(n_jobs)+" jobs to queue "+args.queue+"...")
-    for iov in range(0, n_jobs):
-        lxbatchSubmitJob(args.eosdir, os.path.abspath(args.iovmap), iov, basedir, stage_out_dir, args.queue, args.dryrun) 
+        ## create jobs
+        basedir = os.path.expandvars("$CMSSW_BASE")
+        print("Submitting "+str(n_jobs)+" jobs to queue "+args.queue+"...")
+        for iov in range(0, n_jobs):
+            lxbatchSubmitJob(args.eosdir, os.path.abspath(args.iovmap), iov, basedir, stage_out_dir, args.queue, args.dryrun) 
